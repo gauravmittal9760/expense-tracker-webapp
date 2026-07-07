c09b9d7e226a7b1b0502a53707e805b12889f6fd
gauravmittal9760 <gauravmittal1198141@gmail.com>
2026-07-07 16:11:12 +0530

    Test create_all

--- app.py
+++ app.py
@@ -5289,14 +5289,10 @@ def clear_all_admin_login_records():
 
 
 
-print("AAAA BEFORE CREATE_ALL")
-
 with app.app_context():
+    print("BBB CREATE ALL START")
     db.create_all()
-    print("BBBB CREATE_ALL EXECUTED")
-
-with app.app_context():
-    db.create_all()
+    print("BBB CREATE ALL DONE")
 
 if __name__ == "__main__":
     app.run(debug=True)
